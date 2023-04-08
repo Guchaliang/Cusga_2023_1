4b22f05eaf94c188bbd9d763beae5bdb4d15a58f
Guchaliang <93381304+Guchaliang@users.noreply.github.com>
2023-04-08 21:57:23 +0800

    11

--- cusga_2023_1/Assets/bag/BagList.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName ="Mybag",menuName ="BagData/Mybag")]
-public class BagList : ScriptableObject
-{
-    public List<Itemm> itemList= new List<Itemm>();
-}
